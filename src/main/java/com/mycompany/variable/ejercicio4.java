7b303307248a9cf7a9cddf18ed9afa05c85a10c0
makanaochi2906 <reategui_29_06@hotmail.com>
2020-03-19 22:34:05 -0500

    variable2.1

--- src/main/java/com/mycompany/variable/ejercicio4.java
+++ src/main/java/com/mycompany/variable/ejercicio4.java
@@ -11,12 +11,15 @@ package com.mycompany.variable;
  */
 public class ejercicio4 {
     public static void main(String[] args) {
+        //variable en string
         String  texto = "esto es un hola";
         System.out.println(texto);
         
+        //variable tupo numero en var
         var num = 5;
         System.out.println("esto es un numero = " + num);
         
+        // variable boleana
         var boleana = true;
         System.out.println("esto es  = " + boleana );
     }
